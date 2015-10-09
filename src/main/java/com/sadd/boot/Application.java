78dcc0d3b67f52a8b58179152158fe1e520c70c3
sahori79 <hossainsadd@gmail.com>
2015-10-09 13:20:31 -0400

    sadd

--- src/main/java/com/sadd/boot/Application.java
+++ src/main/java/com/sadd/boot/Application.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package com.sadd.boot;
 
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/sadd/boot/TestController.java
+++ src/main/java/com/sadd/boot/TestController.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package com.sadd.boot;
 
 import java.util.Date;
 
